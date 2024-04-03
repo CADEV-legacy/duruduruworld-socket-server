7760bd1a0b7dd13a71363356d63e053cdc697c28
sana-lazystar <sana.lazystar@gmail.com>
2024-04-03 16:28:13 +0900

    Initialize project

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,4 @@ import express from "express";
 
 const app = express();
 
-app.set("port", process.env.PORT || 3001);
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(app.get("port"), () => {
-  console.log(app.get("port"), "번에서 대기중");
-});
+export { app };
